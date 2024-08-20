fcb2ad126d8d015450ea8be9e4fee0b7b94fab02
selvimurugan2595 <selvimurugan2595@gmail.com>
2024-08-20 10:42:20 +0530

    change branch

--- src/main/java/stringMethodsPractice/ToStringAndToCharArrayAndFormatAndRepeat.java
+++ src/main/java/stringMethodsPractice/ToStringAndToCharArrayAndFormatAndRepeat.java
@@ -6,18 +6,21 @@ public class ToStringAndToCharArrayAndFormatAndRepeat {
 		// TODO Auto-generated method stub
 //Return a string's value:
 		String myStr = "Hello, World!";
+		String myStr1 = "Hello,%s your package within %d minutes";
 		System.out.println(myStr.toString());
-		
+
 //Convert a string to a char array:		
-		char arr[]=myStr.toCharArray();
+		char arr[] = myStr.toCharArray();
 		System.out.println(arr[7]);
-		
-		
+
 //repeat() method with the number of counts we want to repeat the string.		
-		int count =3;
-		
+		int count = 3;
+
 		System.out.println(myStr.repeat(count));
-		
+
+		System.out.println(myStr1.format(myStr1, "Selvi", 2));
+//		System.out.println(myStr1.format(myStr1,2,"Selvi"));
+
 	}
 
 }
