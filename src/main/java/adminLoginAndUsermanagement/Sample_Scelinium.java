dce52dc8df96dc233ad9a5047918db7ca7d824e2
Yugandhar786 <vgyugandhargopi2708@gmail.com>
2024-09-06 23:49:28 +0530

    "Selenium"

--- src/main/java/adminLoginAndUsermanagement/Sample_Scelinium.java
+++ src/main/java/adminLoginAndUsermanagement/Sample_Scelinium.java
@@ -1,6 +1,7 @@
 package adminLoginAndUsermanagement;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;  
   
@@ -9,17 +10,20 @@ public class Sample_Scelinium {
 	public static void main(String[] args) {  
         
 	    // declaration and instantiation of objects/variables  
-	    System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");  
+	    System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\Folder2\\chromedriver.exe");  
 	    WebDriver driver=new ChromeDriver();  
 	      
 	// Launch website  
-	    driver.navigate().to("http://www.google.com/");  
+	    driver.get("http://www.google.com/");  
 	          
 	    // Click on the search text box and send value  
-	    driver.findElement(By.id("lst-ib")).sendKeys("javatpoint tutorials");  
-	          
+	    driver.findElement(By.xpath("//textarea[@aria-label='Search']")).sendKeys("javatpoint tutorials");  
+	    driver.findElement(By.xpath("//textarea[@aria-label='Search']")).sendKeys(Keys.RETURN); 
+	    driver.quit();
 	    // Click on the search button  
-	    driver.findElement(By.name("btnK")).click();  
+//	    driver.findElement(By.name("btnK")).click();  
+	    
+	   
 	      
 	    }  
 
