ecc643829e6acdc54011484cdde6301b24458d43
MurugesanSathiyamoorthi <murugesansathiyamoorthi@gmail.com>
2024-09-08 13:36:01 +0530

    updated dropdown class

--- src/main/java/seleniumBasics/DropDownPractice.java
+++ src/main/java/seleniumBasics/DropDownPractice.java
@@ -198,42 +198,6 @@ public class DropDownPractice {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	}
 
-	public static void dropDownUsingSelectClass() throws Exception {
-		System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\drivers\\chromedriver_125.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.hyrtutorials.com/p/html-dropdown-elements-practice.html");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
-//		WebElement courseDropDown = driver.findElement(By.id("course"));
-//		courseDropDown.click();
-//		driver.findElement(By.xpath("//option[text() = 'Javascript']")).click();
-
-		WebElement courseDropDown = driver.findElement(By.id("course"));
-//		courseDropDown.click();
-
-		Select select = new Select(courseDropDown);
-
-		System.out.println(select.isMultiple());
-
-		select.selectByIndex(3);
-
-//		Thread.sleep(5000);
-		pauseExecution(5);
-
-		select.selectByValue("net");
-
-		Thread.sleep(5000);
-
-		select.selectByVisibleText("Java");
-
-		List<WebElement> courseAllOptions = select.getOptions();
-		for (WebElement each : courseAllOptions) {
-			System.out.println(each.getText());
-		}
-
-	}
-
 	public void chooseOptionByValue(WebElement ele, String value) {
 		Select select = new Select(ele);
 		select.selectByValue(value);
